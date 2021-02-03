11550bb5b6a09bf3a2754735531b84a96f1e2bfe
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-02-03 17:55:50 -0500

    Generate computer choice

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 # game.py
 
+import random
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 
@@ -8,32 +11,41 @@ print("-------------------")
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 print("-------------------")
 
+#
 #asking for user input
-
-x = input("Please choose either 'rock', 'paper', 'scisscors': ")
+#
 
 
 #printing many things separated by a comma
-print("You chose: ", x)
+#print("You chose: ", x)
 
 #string concatenation
-print("You chose: " + x)
+#print("You chose: " + x)
 
 #string interpolation / format string use 
-print(f"You chose: ", {x})
+
+user_choice = input("Please choose either 'rock', 'paper', 'scisscors': ")
 
 
-exit()
 
+print(f"You chose: {user_choice}")
+
+
+
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+
+
+print(f"The computer chose: {computer_choice}")
 
 
 #simulating a computer input
 
-print("The computer chose: 'paper'")
-
-#determining who won
-
-print("-------------------")
-print("Oh, the computer won. It's ok.")
-print("-------------------")
-print("Thanks for playing. Please play again!")
+#print("The computer chose: 'paper'")
+#
+##determining who won
+#
+#print("-------------------")
+#print("Oh, the computer won. It's ok.")
+#print("-------------------")
+#print("Thanks for playing. Please play again!")
