be3d4b0b22a55b4874300b5fbec1857af6a553d0
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-02-03 18:19:31 -0500

    Validate user inputs

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 # game.py
 
 import random
-
+#from random import choice
 
 print("Rock, Paper, Scissors, Shoot!")
 
@@ -30,14 +30,43 @@ user_choice = input("Please choose either 'rock', 'paper', 'scisscors': ")
 
 print(f"You chose: {user_choice}")
 
-
-
 options = ["rock", "paper", "scissors"]
-computer_choice = random.choice(options)
 
+#validate the user selection
+#stop the program and not try to determine the winner if the user choice is invalid
+
+user_choice.lower()
+
+if user_choice not in options:
+    print("OOPS, please choose a valid option!")
+    exit()
+
+
+computer_choice = random.choice(options)
+#computer_choice = choice(options)
 
 print(f"The computer chose: {computer_choice}")
 
+
+
+
+
+#determing who won
+
+if computer_choice == user_choice:
+    print("It's tie!")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("You win! Congrats")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("Oh! The computer won, that's ok!")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("Oh! The computer won, that's ok!")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("You win! Congrats")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("You win! Congrats")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("Oh! The computer won, that's ok!")    
 
 #simulating a computer input
 
