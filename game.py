6c83d97a3406c17514673421680c20e6c03ba9c4
rbeyhum <77794249+rbeyhum@users.noreply.github.com>
2021-02-03 17:40:01 -0500

    Ask user for an input

--- game.py
+++ game.py
@@ -2,3 +2,38 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
+
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#asking for user input
+
+x = input("Please choose either 'rock', 'paper', 'scisscors': ")
+
+
+#printing many things separated by a comma
+print("You chose: ", x)
+
+#string concatenation
+print("You chose: " + x)
+
+#string interpolation / format string use 
+print(f"You chose: ", {x})
+
+
+exit()
+
+
+
+#simulating a computer input
+
+print("The computer chose: 'paper'")
+
+#determining who won
+
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
